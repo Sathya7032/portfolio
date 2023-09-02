09a37c4725972b813b93744453fbe0a485fa78a8
Sathya7032 <116868857+Sathya7032@users.noreply.github.com>
2023-09-02 16:42:58 +0530

    Delete portfolio directory

--- portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index, contactHandler, project
-
-
-urlpatterns = [
-    path('',index,name='index'),
-    path('contact/',contactHandler,name='contactHandler'),
-    path('projects/',project, name='project')
-]
