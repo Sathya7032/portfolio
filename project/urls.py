f84bd5013829603d435bc5ac3fa26feee88ae860
Sathya7032 <116868857+Sathya7032@users.noreply.github.com>
2023-09-14 21:36:25 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('portfolio.urls')),
-    
+    path("ckeditor/", include('ckeditor_uploader.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns+= staticfiles_urlpatterns()
